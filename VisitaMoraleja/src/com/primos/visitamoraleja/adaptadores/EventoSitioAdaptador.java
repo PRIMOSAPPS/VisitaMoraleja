5e6942df7e39efd39f3f837ec41e32cf56388c90
h <h@h-F5SL>
2014-08-30 17:39:06 +0200

    El padre de EventoAdaptador y SitioAdaptador

---
+++ VisitaMoraleja/src/com/primos/visitamoraleja/adaptadores/EventoSitioAdaptador.java
@@ -0,0 +1,32 @@
+package com.primos.visitamoraleja.adaptadores;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.widget.BaseAdapter;
+
+public abstract class EventoSitioAdaptador<T> extends BaseAdapter {
+	protected final Activity actividad;
+	protected final List<T> listaObjetos;
+	
+	public EventoSitioAdaptador(Activity actividad, List<T> listaObjetos) {
+		this.actividad = actividad;
+		this.listaObjetos = listaObjetos;
+	}
+
+	@Override
+	public int getCount() {
+		return listaObjetos.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return listaObjetos.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		
+		return position;
+	}
+}
