5f7c0ff1e94cab8b2a333889435251c54ce237dd
h <h@h.com>
2014-12-14 11:27:56 +0100

    Constantes de tipo y codificación para WebView

---
+++ VisitaMoraleja/src/com/primos/visitamoraleja/constantes/Constantes.java
@@ -0,0 +1,13 @@
+package com.primos.visitamoraleja.constantes;
+
+public class Constantes {
+	/**
+	 * Tipo mime para los webview de las actividades
+	 */
+	public final static String mimeType = "text/html";
+	/**
+	 * Codificacion usada para los webview de las actividades
+	 */
+	public final static String encoding = "UTF-8";
+
+}
