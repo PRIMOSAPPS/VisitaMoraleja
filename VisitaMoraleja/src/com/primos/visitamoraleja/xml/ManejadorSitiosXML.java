22a5004d392622632d2bd460bc8f0f494cdd7a26
h <h@h.com>
2015-12-27 21:15:23 +0100

    Se recupera mas información de los sitios en base 64

--- VisitaMoraleja/src/com/primos/visitamoraleja/xml/ManejadorSitiosXML.java
+++ VisitaMoraleja/src/com/primos/visitamoraleja/xml/ManejadorSitiosXML.java
@@ -53,15 +53,15 @@ public class ManejadorSitiosXML extends DefaultHandler {
 			if(localName.equals("idSitio")) {
 				sitio.setId(Long.parseLong(cadena.toString()));
 			} else if(localName.equals("idCategoria")) {
-					sitio.setIdCategoria(Long.parseLong(cadena.toString()));
+				sitio.setIdCategoria(Long.parseLong(cadena.toString()));
 			} else if(localName.equals("nombre")) {
-				sitio.setNombre(cadena.toString());
+				sitio.setNombre(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("textoCorto1")) {
-				sitio.setTextoCorto1(cadena.toString());
+				sitio.setTextoCorto1(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("textoCorto2")) {
-				sitio.setTextoCorto2(cadena.toString());
+				sitio.setTextoCorto2(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("textoCorto3")) {
-				sitio.setTextoCorto3(cadena.toString());
+				sitio.setTextoCorto3(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("textoLargo1")) {
 				String textoLargoBase64 = cadena.toString();
 				String datosTextoLargo = new String(Base64.decode(textoLargoBase64, Base64.DEFAULT));
@@ -71,26 +71,26 @@ public class ManejadorSitiosXML extends DefaultHandler {
 				String datosTextoLargo = new String(Base64.decode(textoLargoBase64, Base64.DEFAULT));
 				sitio.setTextoLargo2(datosTextoLargo);
 			} else if(localName.equals("nombreLogotipo")) {
-				sitio.setNombreLogotipo(cadena.toString());
+				sitio.setNombreLogotipo(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("logotipo")) {
 //				byte[] strImagenBase64 = cadena.toString().trim().getBytes("UTF-16"); ;
 //				byte[] base64decoded = Base64.decode(strImagenBase64, Base64.DEFAULT);
 //				Bitmap icono = BitmapFactory.decodeByteArray(base64decoded, 0, base64decoded.length);
 				sitio.setLogotipo(ConversionesUtil.getBitmap(cadena));
 			} else if(localName.equals("nombreImagen1")) {
-				sitio.setNombreImagen1(cadena.toString());
+				sitio.setNombreImagen1(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("imagen1")) {
 				sitio.setImagen1(ConversionesUtil.getBitmap(cadena));
 			} else if(localName.equals("nombreImagen2")) {
-				sitio.setNombreImagen2(cadena.toString());
+				sitio.setNombreImagen2(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("imagen2")) {
 				sitio.setImagen2(ConversionesUtil.getBitmap(cadena));
 			} else if(localName.equals("nombreImagen3")) {
-				sitio.setNombreImagen3(cadena.toString());
+				sitio.setNombreImagen3(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("imagen3")) {
 				sitio.setImagen3(ConversionesUtil.getBitmap(cadena));
 			} else if(localName.equals("nombreImagen4")) {
-				sitio.setNombreImagen4(cadena.toString());
+				sitio.setNombreImagen4(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("imagen4")) {
 				sitio.setImagen4(ConversionesUtil.getBitmap(cadena));
 			} else if(localName.equals("latitud")) {
@@ -98,21 +98,21 @@ public class ManejadorSitiosXML extends DefaultHandler {
 			} else if(localName.equals("longitud")) {
 				sitio.setLongitud(Double.parseDouble(cadena.toString()));
 			} else if(localName.equals("direccion")) {
-				sitio.setDireccion(cadena.toString());
+				sitio.setDireccion(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("poblacion")) {
-				sitio.setPoblacion(cadena.toString());
+				sitio.setPoblacion(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("telefonosFijos")) {
-				sitio.setTelefonosFijos(cadena.toString());
+				sitio.setTelefonosFijos(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("telefonosMoviles")) {
-				sitio.setTelefonosMoviles(cadena.toString());
+				sitio.setTelefonosMoviles(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("web")) {
-				sitio.setWeb(cadena.toString());
+				sitio.setWeb(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("email")) {
-				sitio.setEmail(cadena.toString());
+				sitio.setEmail(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("facebook")) {
-				sitio.setFacebook(cadena.toString());
+				sitio.setFacebook(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("twitter")) {
-				sitio.setTwitter(cadena.toString());
+				sitio.setTwitter(stringFromBase64(cadena.toString()));
 			} else if(localName.equals("ranking")) {
 				sitio.setRanking(Integer.parseInt(cadena.toString()));
 			} else if(localName.equals("activo")) {
@@ -127,6 +127,11 @@ public class ManejadorSitiosXML extends DefaultHandler {
 		} catch(Exception e) {
 			throw new SAXException("Error al leer una cadena #" + cadena.toString().trim() + "#", e);
 		}
+	}
+	
+	private String stringFromBase64(String txtBase64) {
+		String textoBase64 = txtBase64.toString();
+		return new String(Base64.decode(textoBase64, Base64.DEFAULT));
 	}
 
 	@Override
