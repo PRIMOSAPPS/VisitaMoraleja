24c70059d302595a17d7c0c4a94c9c23f93cf4a6
h <h@h.com>
2016-03-01 22:08:40 +0100

    Se añade la opción del mapa al menú lateral

--- app/src/main/java/com/primos/visitamoraleja/menulateral/ConfigMenuLateral.java
+++ app/src/main/java/com/primos/visitamoraleja/menulateral/ConfigMenuLateral.java
@@ -2,10 +2,12 @@ package com.primos.visitamoraleja.menulateral;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,18 +15,24 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.primos.visitamoraleja.EventoListaNormalActivity;
+import com.primos.visitamoraleja.MainActivity;
+import com.primos.visitamoraleja.MapaLugaresActivity;
+import com.primos.visitamoraleja.MapsActivity;
 import com.primos.visitamoraleja.NotificacionesActivity;
 import com.primos.visitamoraleja.PreferenciasActivity;
 import com.primos.visitamoraleja.R;
 import com.primos.visitamoraleja.adaptadores.MenuLateralAdaptador;
 import com.primos.visitamoraleja.bdsqlite.datasource.CategoriasDataSource;
+import com.primos.visitamoraleja.bdsqlite.datasource.SitiosDataSource;
 import com.primos.visitamoraleja.contenidos.Categoria;
 
 public class ConfigMenuLateral {
 	private static int IND_FAVORITOS = 5;
+	private static int IND_MAPA = 6;
 	private static int IND_VER_NOTIFICACIONES = -1;
-	private static int IND_PREFERENCIAS = 7;
-	
+	private static int IND_PREFERENCIAS = 8;
+	private static int IND_CREAR_NOTIFICACION = 9;
+
 	private EventoListaNormalActivity actividadSitios;
 	private Activity actividad;
 	private Categoria categoriaSeleccionada;
@@ -81,6 +89,11 @@ public class ConfigMenuLateral {
 		DatosItemMenuLateral datosItemFavoritos = new DatosItemMenuLateral((String)resources.getText(R.string.favoritos),
 				R.mipmap.ic_favoritosml);
 		listaItemsMenu.add(datosItemFavoritos);
+		IND_MAPA = listaItemsMenu.size();
+		DatosItemMenuLateral datosItemPreferenciasMapa = new DatosItemMenuLateral(
+				(String)resources.getText(R.string.map_mapa), R.drawable.ic_mapa);
+		listaItemsMenu.add(datosItemPreferenciasMapa);
+
 //		valoresMenuLateral[i++] = (String)resources.getText(R.string.favoritos);
 		IND_VER_NOTIFICACIONES = listaItemsMenu.size();
 		DatosItemMenuLateral datosItemNotificaciones = new DatosItemMenuLateral(
@@ -92,7 +105,11 @@ public class ConfigMenuLateral {
 				(String)resources.getText(R.string.actionbar_settings), R.drawable.ic_action_action_settings);
 		listaItemsMenu.add(datosItemPreferencias);
 //		valoresMenuLateral[i++] = (String)resources.getText(R.string.actionbar_settings);
-		
+//		IND_CREAR_NOTIFICACION = listaItemsMenu.size();
+//		DatosItemMenuLateral datosItemPreferenciasNotificacion = new DatosItemMenuLateral(
+//				"Crear Notif", R.drawable.ic_action_action_settings);
+//		listaItemsMenu.add(datosItemPreferenciasNotificacion);
+
 		ListView mDrawerOptions = (ListView) actividadRecibida.findViewById(R.id.menuLateralListaSitios);
 		final DrawerLayout mDrawer = (DrawerLayout) actividadRecibida.findViewById(R.id.drawer_layout_lateral);
 		
@@ -133,6 +150,34 @@ public class ConfigMenuLateral {
 						intent.putExtra(NotificacionesActivity.CATEGORIA_NOTIFICACIONES, categoriaSeleccionada.getId());
 					}
 					actividadRecibida.startActivity(intent);
+				} else if(indice == IND_MAPA) {
+					/*
+					Intent lanzarmapa = new Intent(actividadRecibida, MapaLugaresActivity.class);
+
+					//String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?saddr=%f,%f&daddr=%f,%f", sourceLatitude, sourceLongitude, destinationLatitude, destinationLongitude);
+					String uri = "http://maps.google.com/maps?saddr=40.068782,-6.6562358";
+					Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+
+					// Vamos a pasar los valores que necesita MapaLugarActivity
+					lanzarmapa.putExtra("nombre", "Nombre del sitio");
+					lanzarmapa.putExtra("latitud", 40.068782);
+					lanzarmapa.putExtra("longitud", -6.6562358);
+					lanzarmapa.putExtra(MapaLugaresActivity.ORIGEN, "SITIO");
+					lanzarmapa.putExtra(MapaLugaresActivity.ID_RECIBIDO, 9);
+
+					actividadRecibida.startActivity(lanzarmapa);
+				} else if(indice == IND_MAPA2) {
+					*/
+					Intent lanzarmapa = new Intent(actividadRecibida, MapsActivity.class);
+
+					//String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?saddr=%f,%f&daddr=%f,%f", sourceLatitude, sourceLongitude, destinationLatitude, destinationLongitude);
+					String uri = "http://maps.google.com/maps?saddr=40.068782,-6.6562358";
+					Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+
+					actividadRecibida.startActivity(lanzarmapa);
+//				} else if(indice == IND_CREAR_NOTIFICACION) {
+//					MainActivity ma = (MainActivity)actividadRecibida;
+//					ma.crearNotificaciones(null);
 				}
 				mDrawer.closeDrawers();
 			}
