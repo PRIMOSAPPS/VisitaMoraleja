42c130c46a7164cf8eda4ef0d23c48bb40e9fe50
h <h@h.com>
2016-03-03 19:52:18 +0100

    Se adapta el tamaño del TAG para los logs

--- app/src/main/java/com/primos/visitamoraleja/VisitaMoralejaApplication.java
+++ app/src/main/java/com/primos/visitamoraleja/VisitaMoralejaApplication.java
@@ -10,7 +10,7 @@ import com.primos.visitamoraleja.util.UtilPropiedades;
 
 
 public class VisitaMoralejaApplication extends Application {
-	private final static String TAG = "[VisitaMoralejaApplication]";
+	private final static String TAG = "[VisitaMoralejaApplica]";
 	
 	public VisitaMoralejaApplication() {
 		super();
@@ -46,12 +46,12 @@ public class VisitaMoralejaApplication extends Application {
 
 	private void registraParse() {
 		String  androidId = Secure.getString(getApplicationContext().getContentResolver(),Secure.ANDROID_ID);
-		Log.d("VisitaMoralejaApplication", "ANDROID_ID El id conseguido es: " + androidId);
+		Log.d(TAG, "ANDROID_ID El id conseguido es: " + androidId);
 		UtilPropiedades propiedades = UtilPropiedades.getInstance();
 		String applicationId = propiedades.getProperty(UtilPropiedades.PROP_PARSE_APPLICATION_ID); 
         String clientKey = propiedades.getProperty(UtilPropiedades.PROP_PARSE_CLIENT_KEY);
-        Log.d("VisitaMoralejaApplication", "applicationId: " + applicationId);
-        Log.d("VisitaMoralejaApplication", "clientKey: " + clientKey);
+        Log.d(TAG, "applicationId: " + applicationId);
+        Log.d(TAG, "clientKey: " + clientKey);
 
 		Parse.initialize(this, applicationId, clientKey);
 //		PushService.setDefaultPushCallback(this, MainActivity.class);
