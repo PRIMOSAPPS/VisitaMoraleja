e0384cb8ef6493da068d01003ce1929f88021ea0
h <h@h.com>
2016-06-26 19:54:51 +0200

    Se buscan solo los que pertenecen al evento

--- app/src/main/java/com/primos/visitamoraleja/actividades/eventos/ListaActividadesEventoActivity.java
+++ app/src/main/java/com/primos/visitamoraleja/actividades/eventos/ListaActividadesEventoActivity.java
@@ -1,5 +1,7 @@
 package com.primos.visitamoraleja.actividades.eventos;
 
+import android.os.Bundle;
+
 import com.primos.visitamoraleja.R;
 import com.primos.visitamoraleja.actividades.ListaGeneralActivity;
 import com.primos.visitamoraleja.adaptadores.ActividadEventoAdaptador;
@@ -16,6 +18,8 @@ import java.util.List;
  * Created by h on 25/06/16.
  */
 public class ListaActividadesEventoActivity extends ListaGeneralActivity {
+    private long idEvento;
+
     @Override
     protected AbstractDataSource createDataSource() {
         return new ActividadEventoDataSource(this);
@@ -23,12 +27,19 @@ public class ListaActividadesEventoActivity extends ListaGeneralActivity {
 
     @Override
     protected void cargar() {
-        List<ActividadEvento> lista = ((ActividadEventoDataSource)dataSource).getAll();
+        List<ActividadEvento> lista = ((ActividadEventoDataSource)dataSource).getByIdEvento(idEvento);
         setListAdapter(new ActividadEventoAdaptador(this, lista));
     }
 
     @Override
     protected String getTitulo() {
         return getResources().getString(R.string.actividades);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        idEvento = (long) getIntent().getExtras().get(EventoPrincipalActivity.ID_EVENTO);
+        super.onCreate(savedInstanceState);
     }
 }
--- app/src/main/java/com/primos/visitamoraleja/actividades/eventos/ListaSitiosEventoActivity.java
+++ app/src/main/java/com/primos/visitamoraleja/actividades/eventos/ListaSitiosEventoActivity.java
@@ -1,5 +1,7 @@
 package com.primos.visitamoraleja.actividades.eventos;
 
+import android.os.Bundle;
+
 import com.primos.visitamoraleja.R;
 import com.primos.visitamoraleja.actividades.ListaGeneralActivity;
 import com.primos.visitamoraleja.adaptadores.SitioEventoAdaptador;
@@ -13,6 +15,8 @@ import java.util.List;
  * Created by h on 25/06/16.
  */
 public class ListaSitiosEventoActivity extends ListaGeneralActivity {
+    private long idEvento;
+
     @Override
     protected AbstractDataSource createDataSource() {
         return new SitioEventoDataSource(this);
@@ -20,12 +24,18 @@ public class ListaSitiosEventoActivity extends ListaGeneralActivity {
 
     @Override
     protected void cargar() {
-        List<SitioEvento> lista = ((SitioEventoDataSource)dataSource).getAll();
+        List<SitioEvento> lista = ((SitioEventoDataSource)dataSource).getByIdEvento(idEvento);
         setListAdapter(new SitioEventoAdaptador(this, lista));
     }
 
     @Override
     protected String getTitulo() {
         return getResources().getString(R.string.sitios);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        idEvento = (long) getIntent().getExtras().get(EventoPrincipalActivity.ID_EVENTO);
+        super.onCreate(savedInstanceState);
     }
 }
